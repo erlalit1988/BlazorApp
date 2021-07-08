3384f6656c388718c87e70c4f9f0eff8c0baf06e
lalit Kumae <lalit.k9956@gmail.com>
2021-07-08 19:40:43 +0530

    New banana Service Added

---
+++ src/BlazorBattles/BlazorBattles/Client/Services/BananaService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services
+{
+    public class BananaService : IBananaService
+    {
+        public int Bananas { get; set; } = 1000;
+
+        public event Action OnChange;
+
+        public void AddBananas(int amount)
+        {
+            Bananas += amount;
+            BananaChanged();
+        }
+
+        public void EatBananas(int amount)
+        {
+            Bananas -= amount;
+            BananaChanged();
+        }
+
+        void BananaChanged() => OnChange.Invoke();
+    }
+}
