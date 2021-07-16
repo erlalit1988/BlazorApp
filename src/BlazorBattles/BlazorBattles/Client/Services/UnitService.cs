d9d2228a4c6444aac8bf9ba0d5b13b8774ccd88f
lalit Kumae <lalit.k9956@gmail.com>
2021-07-16 19:23:06 +0530

    Add Army Component n Blazored toast notification

--- src/BlazorBattles/BlazorBattles/Client/Services/UnitService.cs
+++ src/BlazorBattles/BlazorBattles/Client/Services/UnitService.cs
@@ -11,8 +11,8 @@ namespace BlazorBattles.Client.Services
         public IList<Unit> Units => new List<Unit>
         {
             new Unit{ Id=1, Title="Knight", Attack =10, Defense=10, BananaCost=100},
-            new Unit{ Id=1, Title="Archer", Attack =15, Defense=10, BananaCost=150},
-            new Unit{ Id=1, Title="Mage", Attack =20, Defense=1, BananaCost=200}
+            new Unit{ Id=2, Title="Archer", Attack =15, Defense=10, BananaCost=150},
+            new Unit{ Id=3, Title="Mage", Attack =20, Defense=1, BananaCost=200}
 
         };
         // public IList<Unit> Units { get; set; }
@@ -22,8 +22,8 @@ namespace BlazorBattles.Client.Services
         {
             var unit = Units.First(unit => unit.Id == unitId);
             MyUnits.Add(new UserUnit { UnitId = unit.Id, HitPoints = unit.HitPoints });
-            Console.WriteLine($"{unit.Title} was built!");
-            Console.WriteLine($"Your army size: { MyUnits.Count}");
+           // Console.WriteLine($"{unit.Title} was built!");
+            //Console.WriteLine($"Your army size: { MyUnits.Count}");
 
         }
     }
