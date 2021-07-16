8e6f497c8badf90267a43f27bcf22848f31be4f4
lalit Kumae <lalit.k9956@gmail.com>
2021-07-16 17:15:30 +0530

    Added Interface and Dtos

--- src/Asp_NetRestAPI/Catalog/Controllers/ItemsContoller.cs
+++ src/Asp_NetRestAPI/Catalog/Controllers/ItemsContoller.cs
@@ -1,11 +1,42 @@
+using System.Linq;
+using System;
+using Catalog.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Catalog.Entities;
+using System.Collections.Generic;
+using Catalog.Dtos;
 
 namespace Catalog.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("Items")]
     public class ItemsContoller:ControllerBase
     {
-        
+       // private readonly InMemItemRepository repository;
+        private readonly IItemsRepository itmesRepo;
+
+        public ItemsContoller(IItemsRepository itmesRepo)
+        {
+            //repository = new InMemItemRepository();
+            this.itmesRepo = itmesRepo;
+        }
+        //Get/items
+        [HttpGet]
+        public IEnumerable<ItemDto> GetItmes()
+        {
+            var items= itmesRepo.GetItems().Select(x => x.AsDto());
+            return items;
+        }
+        //Get/items/{id}
+         [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetItme(Guid id)
+        {
+            var item=itmesRepo.GetItem(id);
+            if(item is null)
+            {
+                return NotFound();
+            }
+            return item.AsDto();
+        }
     }
 }
--- src/Asp_NetRestAPI/Catalog/Repositories/InMemItemRepository.cs
+++ src/Asp_NetRestAPI/Catalog/Repositories/InMemItemRepository.cs
@@ -5,25 +5,31 @@ using Catalog.Entities;
 
 namespace Catalog.Repositories
 {
-    public class InMemItemRepository
+    public class InMemItemRepository : IItemsRepository
     {
         private readonly List<Item> items = new()
         {
-              new Item{ Id=Guid.NewGuid(), Name="Potion", Price=9, CreateDate=DateTimeOffset.UtcNow },
-              new Item{ Id=Guid.NewGuid(), Name="Iron Sord", Price=20, CreateDate=DateTimeOffset.UtcNow },
-              new Item{ Id=Guid.NewGuid(), Name="Bronze Shield", Price=18, CreateDate=DateTimeOffset.UtcNow }
+            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreateDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sord", Price = 20, CreateDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreateDate = DateTimeOffset.UtcNow }
         };
         public InMemItemRepository()
         {
-            
+
         }
+
+        public InMemItemRepository(List<Item> items)
+        {
+            this.items = items;
+        }
+
         public IEnumerable<Item> GetItems()
         {
             return items;
         }
         public Item GetItem(Guid id)
         {
-            return items.Where(item => item.Id==id).SingleOrDefault();
+            return items.Where(item => item.Id == id).SingleOrDefault();
         }
     }
 }
